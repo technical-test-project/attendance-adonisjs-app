20472fb7edfbe40055267ead1e8699d61d62ba4f
Alvin Setiawan <alvin.set25@gmail.com>
2024-03-30 02:39:06 +0700

    Update attendance_seeder.ts

--- database/seeders/attendance_seeder.ts
+++ database/seeders/attendance_seeder.ts
@@ -10,17 +10,19 @@ export default class extends BaseSeeder {
     const end = DateTime.fromISO('2024-03-10')
     const numbers = [1, 2, 3, 4, 5]
     for (let i = 0, date = start; date <= end; date = date.plus({ day: 1 })) {
-      let userId = Math.floor(Math.random() * numbers.length) + 1
+      let randomNumber = Math.floor(Math.random() * numbers.length) + 1
       let status = Attendance.CLOCK_IN
-      let clockAt = DateTime.now().plus({ day: 1, minute: 5 })
+      let clockAt = date.plus({ hour: randomNumber, minute: randomNumber, second: randomNumber })
       if (i % 2 === 0) {
         status = Attendance.CLOCK_OUT
-        clockAt = DateTime.now().plus({ day: 1, hour: 5, minute: 5 })
+        clockAt = clockAt.plus({ hour: 5, minute: 5 })
       }
       await Attendance.create({
-        userId: userId,
+        userId: randomNumber,
         status: status,
         clockAt: clockAt,
+        createdAt: date,
+        updatedAt: date,
       })
     }
   }
