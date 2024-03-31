5a62857ef781f6684d5bdd0ebbaf178ae2ed70c2
Alvin Setiawan <alvin.set25@gmail.com>
2024-03-31 08:59:27 +0700

    Update user_service.ts

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -123,7 +123,7 @@ export default class UserService {
 
       // Store Photo
       await payload.photo?.move(app.publicPath(User.table), {
-        name: `${user.uuid}.jpg`,
+        name: `${User.table}${user.uuid}.jpg`,
         overwrite: true,
       })
 
